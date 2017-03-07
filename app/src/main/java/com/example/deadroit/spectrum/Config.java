f782713d946bcaae3d82f6f25673967ba2f09f61
Pramudya Erviansyah <vizyan.1@gmail.com>
2017-03-07 07:42:41 +0700

    First commit

---
+++ app/src/main/java/com/example/deadroit/spectrum/Config.java
@@ -0,0 +1,29 @@
+package com.example.deadroit.spectrum;
+
+/**
+ * Created by Deadroit on 1/17/2017.
+ */
+
+public class Config {
+    public static final String URL_ADD = "http://beta.skylightmod.web.id/CRUD/addEmp.php";
+    public static final String URL_GET_ALL = "http://beta.skylightmod.web.id/CRUD/getAllEmp.php";
+    public static final String URL_GET_EMP = "http://beta.skylightmod.web.id/CRUD/getEmp.php?id=";
+    public static final String URL_UPDATE_EMP = "http://beta.skylightmod.web.id/CRUD/updateEmp.php";
+    public static final String URL_DELETE_EMP = "http://beta.skylightmod.web.id/CRUD/deleteEmp.php?id=";
+    public static final String URL_SEND_MULTIPLE_PUSH = "http://beta.skylightmod.web.id/firebase/sendMultiplePush.php";
+
+    public static final String KEY_EMP_ID = "id";
+    public static final String KEY_EMP_NAME = "name";
+    public static final String KEY_EMP_DESG = "desg";
+    public static final String KEY_EMP_SAL = "salary";
+    public static final String KEY_EMP_TIME = "time";
+
+    public static final String TAG_JSON_ARRAY = "result";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAME = "name";
+    public static final String TAG_DESG = "desg";
+    public static final String TAG_SAL = "salary";
+    public static final String TAG_TIME = "time";
+
+    public static final String EMP_ID = "emp_id";
+}
