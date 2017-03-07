57ce483bd93eece3db1b8a4a281d05b0031f752a
Pramudya Erviansyah <vizyan.1@gmail.com>
2017-03-07 07:43:06 +0700

    First commit

---
+++ app/src/main/java/com/example/deadroit/spectrum/DataFish.java
@@ -0,0 +1,10 @@
+package com.example.deadroit.spectrum;
+
+public class DataFish {
+
+    public String fishName;
+    public String catName;
+    public String sizeName;
+    public String price;
+    public int harga;
+}
